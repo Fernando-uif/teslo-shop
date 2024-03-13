09f604c879a1f0c35a0e027c63db2db93f7cf6b8
Fernando Sosa Torres <fsosat1400@alumno.ipn.mx>
2024-03-12 19:50:36 -0600

    feat(app): E-commerce
    Add the category page and validations

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -1,10 +1,13 @@
+import { TopMenu } from "@/components";
+
 export default function ShopLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <div className=" min-h-screen bg-red-500">
+    <div className=" min-h-screen ">
+      <TopMenu />
       <h1>{children}</h1>
     </div>
   );
